117e13b6b45295709f8c6f1b2e23e822ed4538bc
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2023-05-10 18:15:08 -0700

    More WIP

--- iocBoot/iocEK9KTest/motor.cmd
+++ iocBoot/iocEK9KTest/motor.cmd
@@ -1,4 +1,6 @@
-dbpf IOC:m1.SSET 1
-dbpf IOC:m1.SET 1
-dbpf IOC:m1.DVAL 10
-dbpf IOC:m1.SPMG 3
+#!/usr/bin/env bash
+
+caput IOC:m1.SSET 1
+caput IOC:m1.SET 1
+caput IOC:m1.DVAL 10
+caput IOC:m1.SPMG 3
